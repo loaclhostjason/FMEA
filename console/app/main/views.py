7ce5334c110743d12cfe45fc31819dab8dc51fb3
zhangjie <951995314@qq.com>
2018-05-09 00:11:18 +0800

    first commit

---
+++ console/app/main/views.py
@@ -0,0 +1,18 @@
+# coding: utf-8
+from flask import render_template, redirect, url_for, jsonify, abort, request, flash
+from flask_login import login_required
+
+from . import main
+
+
+@main.route('/main')
+@main.route('/')
+@login_required
+def my_file_list():
+    return render_template('main/my_file.html')
+
+
+@main.route('/main/file/create')
+@login_required
+def create_file():
+    return render_template('main/create_file.html')
